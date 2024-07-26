65c9fc68fe72b72c3dace130b699170135d14702
Prithvi Singh <93911560+PrithviSingh0112@users.noreply.github.com>
2024-07-26 14:07:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import "./App.css";
+import WeatherSummary from "./components/WeatherSummary.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherSummary />
+    </div>
+  );
+}
+
+export default App;
